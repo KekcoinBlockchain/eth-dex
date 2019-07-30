147486245914557483fd958d362efb0459b67aa2
subzero <chaxz93@gmail.com>
2019-07-30 16:24:41 +0300

    :wrench: config(Migration): migration of exchange smart contract added for truffle

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
 const Token = artifacts.require("Token");
+const Exchange = artifacts.require("Exchange");
 
-module.exports = function(deployer) {
-  deployer.deploy(Token);
+module.exports = async function(deployer) {
+
+	const accounts = await web3.eth.getAccounts();
+
+	await deployer.deploy(Token);
+
+	const feeReceiver = accounts[2];
+	const makerFee = 1;
+	const takerFee = 2;
+
+	await deployer.deploy(Exchange, feeReceiver, makerFee, takerFee);
 };
