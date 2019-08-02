6b7d58b989e5a8d03c82eb3f2f81eb6c881aed94
subzero <chaxz93@gmail.com>
2019-08-03 02:41:53 +0300

    removed blockchain params

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Web3 from 'web3'
-
 
 class App extends Component {
-  componentWillMount() {
-    this.loadBlockchainData()
-  }
-
-  async loadBlockchainData() {
-    const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545')
-    const network = await web3.eth.net.getNetworkType()
-    const networkId = await web3.eth.net.getId()
-    const accounts = await web3.eth.getAccounts()
-  }
 
   render() {
     return (
